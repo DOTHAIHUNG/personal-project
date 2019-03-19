24b2fedf0862cad3f63513157a107dc9d4af4c2d
thefourhorsemen-4 <fourhorsemen987@gmail.com>
2019-03-19 10:18:02 +0700

    first deploy to git and heroku

---
+++ src/main/java/com/league/bloglol/BlogLolApplication.java
@@ -0,0 +1,13 @@
+package com.league.bloglol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogLolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogLolApplication.class, args);
+    }
+
+}
